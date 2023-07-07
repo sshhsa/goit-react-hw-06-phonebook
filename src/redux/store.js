23c2746027bb9bc8d952ed441f2ec4e751015ac2
Olexandr <118224486+sshhsa@users.noreply.github.com>
2023-07-07 22:44:27 +0300

    Installed library persist for saving data by local storage

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,26 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import { contactsReducer } from './contactsSlice';
 import { filtersReducer } from './filtersSlice';
 
-export const store = configureStore({
-  reducer: { contacts: contactsReducer, filters: filtersReducer },
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filters: filtersReducer,
 });
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whiteList: ['contacts'],
+};
+
+const persistedRootReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedRootReducer,
+});
+
+export const persistor = persistStore(store);
